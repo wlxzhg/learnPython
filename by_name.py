9ccbf3d07103802f7f058908de2dd3c6227058c5
wlxzhg <xiaozhigangcn@163.com>
2016-10-17 22:48:00 +0800

    装饰器

--- by_name.py
+++ by_name.py
@@ -2,4 +2,4 @@ def by_name(t):
 	return t[0].lower()
 
 def by_score(t):
-	return t[1].lower()
+	return t[1]
--- log.py
+++ log.py
@@ -1,3 +1,4 @@
+#装饰器学习
 import functools
 
 def log(func):
@@ -6,9 +7,36 @@ def log(func):
 		print('begin call %s():' % func.__name__)
 		func(*args,**kw)
 		print('end call %s():' % func.__name__)
-	#print('end call %s():' % func.__name__)
 	return wrapper
 
 @log
 def now():
 	print('2016-10-16')
+
+def log1(arg):
+	func = arg
+	if(isinstance(arg,str)):
+		def decorator(func):
+			@functools.wraps(func)
+			def wrapper(*args,**kw):
+				print('%s %s():' % (arg,func.__name__))
+				return func(*args,**kw)
+			return wrapper
+		return decorator
+	else:
+		@functools.wraps(func)
+		def wrapper(*args,**kw):
+			print('begin call %s():' % func.__name__)
+			func(*args,**kw)
+			print('end call %s():' % func.__name__)
+		return wrapper
+
+#now1 = log1(now1)
+@log1
+def now1():
+	print('now1:2016-10-17')
+
+#now2 = log('execute')(now2)
+@log1('execute')
+def now2():
+	print('now2:2016-10-17')
